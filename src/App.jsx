93c6dc76f467b571b9ad4e637ae16d93ff809684
Angel Castaneda <luis.castaneda@noktos.com>
2024-12-24 11:17:49 -0600

    DONE ROUTER

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Main } from "./components/Main/Main.jsx";
 import { UploadFiles } from "./views/upload_files/UploadFiles.jsx";
 import { Prueba } from "./views/prueba/Prueba.jsx";
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 
 function App() {
     return (
         <>
-            <Switch>
-                <Route path="/" component={Main} />
-                <Route path="/upload" component={UploadFiles} />
-                <Route path="/prueba" component={Prueba} />
-            </Switch>
+            <Router>
+                <Switch>
+                    <Route path="/" component={Main} />
+                    <Route path="/upload" component={UploadFiles} />
+                    <Route path="/prueba" component={Prueba} />
+                </Switch>
+            </Router>
         </>
     )
 }
